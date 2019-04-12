bd62e0c146f564d688d70c985fac0689e7985612
rhgkswns21 <rhgkswns@naver.com>
2019-04-12 11:41:36 +0900

    Login, Logout Bug fixe

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'&accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 #    path('admin/', admin.site.urls),
 #    path('', include('blog.urls'))
